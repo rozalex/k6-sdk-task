833e7e95dbd57b460007bbea287c3d6ba06b5954
Alex Rozhavsky <alex.rozhavsky@priority-software.com>
2025-01-21 18:18:32 +0200

    add readme

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,7 @@ import { Options } from 'k6/options';
 import * as sdk from "./priority-web-sdk";
 import { loginWebSdk } from './priority-web-sdk/login';
 
-/*
-  Priority web sdk documentation: https://prioritysoftware.github.io/api/
 
-  Instructions and hints:
-    1. You don't need to install priority web sdk. its is already inside this project. just import it like the example above.
-    2. To run the project, install and then run k6 using npm run test
-    3. ChatGPT will confuse you. Refer to the documentation. It is not very clear but has everything you need.
-
-    Good luck!
-*/
 export let options:Options = {
   vus: 1,
   duration: '10s'
